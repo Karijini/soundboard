a64e47323eb47e025e1fc7bf4ca6e58e8cf32518
Sebastian Thinnes <sebastian.thinnes@gmx.de>
2016-03-27 13:18:58 +0000

    changes

--- zmq_client.py
+++ zmq_client.py
@@ -6,10 +6,14 @@ import zmq
 if __name__ == '__main__':
     port = 5555
     ip = 'python-timer.rhcloud.com'
-    ip = '192.168.0.18'
+    ip = '127.0.0.1'
     context = zmq.Context()
     print "Connecting to server..."
     socket = context.socket(zmq.REQ)
     socket.connect("tcp://%s:%s" % (ip,port))
-    socket.send ("Hello")
+    socket.send ("listSounds")
+    print socket.recv()
+    socket.send ("playSound:intro")
+    print socket.recv()
+    socket.send ("playSound:level_up")
     print socket.recv()
--- zmq_server.py
+++ zmq_server.py
@@ -1,54 +1,70 @@
-#!/usr/bin/python
-import os
-import time
+import ConfigParser
 import pygame
-
-# virtenv = os.environ['OPENSHIFT_PYTHON_DIR'] + '/virtenv/'
-# virtualenv = os.path.join(virtenv, 'bin/activate_this.py')
-# try:
-#     execfile(virtualenv, dict(__file__=virtualenv))
-# except IOError:
-#     pass
-#
+import os
+from collections import OrderedDict
 import zmq
 
-def list_sounds():
-    sounds = []
-    for item in os.listdir('sounds'):
-        if item.endswith('.wav'):
-            sounds.append(item)
-    return ','.join(sounds)
-
-
-class Server(object):
-    def __init__(self, ip, port):
+class Soundboard(object):
+    def __init__(self,ip,port):
         self.__ip = ip
         self.__port = port
-        self.__ctx = None
-        self.__socket = None
-        self.__sounds_path = 'sounds'
-        self.__sounds = {}
-        pygame.mixer.init()
+        self.__mixer=pygame.mixer.init()
+        self.__sounds = OrderedDict()
 
-    def load_sounds(self):
-        sounds = {}
-        for item in os.listdir(self.__sounds_path):
-            if item.endswith('.wav'):
-                sound_file_path = os.path.join(self.__sounds_path,item)
-                sound = pygame.mixer.Sound(sound_file_path)
-                length = sound.get_length()
-                sounds[item] = {'sound':sound,
-                                'length':length}
-        return sounds
+    def __load_sounds(self):
+        self.__sounds.clear()
+        cfg = ConfigParser.ConfigParser()
+        cfg.readfp(open('sounds.cfg'))
+        for section in cfg.sections():
+            print section
+            if not cfg.has_option(section,"file"):
+                continue
+            sound_file_path = os.path.join('sounds', cfg.get(section,'file'))
+            if not os.path.isfile(sound_file_path):
+                print 'ignoring',sound_file_path
+                
+            sound = pygame.mixer.Sound(sound_file_path)
+            length = sound.get_length()
+            _in = 0
+            if cfg.has_option(section,'in'):
+                _in = cfg.getfloat(section,'in')
+            _out = length
+            if cfg.has_option(section,'out'):
+                __out = cfg.getfloat(section,'out')
+                if __out < _out:
+                    _out = __out
+            if _in!=0 or _out!=length:
+                print sound
+                a1 = pygame.sndarray.samples(sound)
+                samples_per_second = pygame.mixer.get_init()[0]
+                start_pos_in_samples = int(_in * samples_per_second)
+                end_pos_in_samples = int(_out * samples_per_second)
+                print start_pos_in_samples, end_pos_in_samples
+                a2 = a1[start_pos_in_samples:end_pos_in_samples]
+                
+                sound = pygame.sndarray.make_sound(a2)
+            print section,sound_file_path, sound.get_length()
 
-    def serve(self):
+            self.__sounds[section] = {'sound':sound,
+                                      'length':sound.get_length(),
+                                      }
+            self.__sounds[section]['fade_in']= 0
+            if cfg.has_option(section,'fade_in'):
+                self.__sounds[section]['fade_in']=cfg.getfloat(section,'fade_in')
+            self.__sounds[section]['fade_out']= 0
+            if cfg.has_option(section,'fade_out'):
+                self.__sounds[section]['fade_out']=cfg.getfloat(section,'fade_out')
+
+        self.__sounds['stop']={'length':500}
+    
+    def start(self):
         self.__ctx = zmq.Context()
         a = "tcp://%s:%s" % (self.__ip,self.__port)
         self.__socket = self.__ctx.socket(zmq.REP)
         self.__socket.bind(a)
         print a
         print 'loading sounds...'
-        self.__sounds = self.load_sounds()
+        self.__load_sounds()
         while True:
             #  Wait for next request from client
             message = self.__socket.recv()
@@ -57,39 +73,32 @@ class Server(object):
             if message == "listSounds":
                 self.__socket.send("listSoundsResult:%s"%(','.join([item for item in self.__sounds])))
             elif message.startswith("playSound:"):
-                sound = message.strip('playSound:')
-                snd1 = pygame.mixer.Sound('sounds/%s'%sound)
-                channel = snd1.play()
-                print snd1.get_length()
-                self.__socket.send("playSoundResult:%s:%f"%(sound,snd1.get_length()))
+                sound_name = message.split('playSound:')[1]
+                if sound_name == "stop":
+                    self.stop_sounds()
+                    length = 1.0
+                else:
+                    length = self.play_sound(sound_name)
+                self.__socket.send("playSoundResult:%s:%f"%(sound_name,length))
+
+    def play_sound(self, sound_name):
+        sound = self.__sounds.get(sound_name,None)
+        print self.__sounds.keys()
+        print sound
+        if sound == None:
+            return False
+        sound['sound'].play(fade_ms=int(sound['fade_in']*1000))
+        return sound['length']
+
+    def stop_sounds(self):
+        pygame.mixer.fadeout(self.__sounds['stop']['length'])
 
 
-if __name__ == '__main__':
-
-    server = Server('*',5555)
-    server.serve()
-    #
-    # port = 5555
-    # ip = '*'
-    # if 'OPENSHIFT_PYTHON_IP' in os.environ:
-    #     ip = os.environ.get('OPENSHIFT_PYTHON_IP')
-    # #ip = '54.88.2.59'
-    # context = zmq.Context()
-    # socket = context.socket(zmq.REP)
-    # a = "tcp://%s:%s" % (ip,port)
-    # print a
-    # socket.bind(a)
-    # #socket.bind("tcp://127.0.0.1:%s" % port)
+if __name__ == "__main__":
+    # soundboard = Soundboard()
+    # print soundboard.get_sounds()
+    # soundboard.play_sound('intro')
     # while True:
-    #     #  Wait for next request from client
-    #     message = socket.recv()
-    #     print message
-    #
-    #     if message == "listSounds":
-    #         socket.send("listSoundsResult:%s"%(list_sounds()))
-    #     elif message.startswith("playSound:"):
-    #         sound = message.strip('playSound:')
-    #         snd1 = pygame.mixer.Sound('sounds/%s'%sound)
-    #         channel = snd1.play()
-    #         print snd1.get_length()
-    #         socket.send("playSoundResult:%s:%f"%(sound,snd1.get_length()))
+    #     pass
+    server = Soundboard('*',5555)
+    server.start()
